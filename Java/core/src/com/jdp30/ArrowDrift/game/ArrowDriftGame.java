61be2bb69e9d1723f9bf293770bd1af26d2e309a
Jack Patrick <jackpatrickmail@gmail.com>
2018-04-24 13:43:12 +0100

    Added the initial version of the website.

--- Java/core/src/com/jdp30/ArrowDrift/game/ArrowDriftGame.java
+++ Java/core/src/com/jdp30/ArrowDrift/game/ArrowDriftGame.java
@@ -1,29 +1,13 @@
 package com.jdp30.ArrowDrift.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.jdp30.ArrowDrift.game.GUI.Callback;
-import com.jdp30.ArrowDrift.game.GUI.ImgButton;
-import com.jdp30.ArrowDrift.game.Level.AllowedMovementType;
-import com.jdp30.ArrowDrift.game.Level.Level;
-import com.jdp30.ArrowDrift.game.Level.Tile.Tile;
-import com.jdp30.ArrowDrift.game.LevelEditor.EditorHomeScreen;
-import com.jdp30.ArrowDrift.game.Screens.InGameScreen;
+
 import com.jdp30.ArrowDrift.game.Screens.MainMenuScreen;
-import org.omg.PortableInterceptor.INACTIVE;
+
 
 public class ArrowDriftGame extends Game {
 
@@ -42,8 +26,6 @@ public class ArrowDriftGame extends Game {
     public void create() {
         font = new BitmapFont(Gdx.files.internal("fonts/cg12.fnt"),Gdx.files.internal("fonts/cg12.png"),false);
         batch = new SpriteBatch();
-       //  setScreen(new InGameScreen());
-     //   setScreen(new EditorHomeScreen());
         setScreen(new MainMenuScreen());
     }
 
